42d43dec5f99b79fa2224a8462aa6e04350ecfb2
Ellis Fangmann <168651076+efang01@users.noreply.github.com>
2026-04-22 15:35:30 -0500

    Add files via upload

--- Calibration Analysis of Data Sets.R
+++ Calibration Analysis of Data Sets.R
@@ -8,7 +8,7 @@
 ################################################################################
 
 # Load and prepare data
-inputpvp<-read.csv("1ChannelDataExpDAY.csv", stringsAsFactors = F)
+inputpvp<-read.csv("4ChannelData2ExpDAY.csv", stringsAsFactors = F)
 x<-(inputpvp$SandFlux)
 y<-(inputpvp$ProbeFlux)
 
@@ -20,18 +20,26 @@ m <- round(regcurrent$coefficients[2], 4)  # slope
 
 # Vector for calculating confidence intervals
 xconfint<-seq(0,70,by=0.0001)
-
 conf_interval <- predict(regcurrent, newdata=(data.frame(x=xconfint)), 
                          interval="confidence", level = 0.95)
 par(mfrow=c(1,2))
 
 # Plot Calibration line
-plot(x,y, xlab="Outside Well", ylab="In Probe",
-     pch=16,cex.main=2,main=paste("Calibration Line"))
+plot(x,y,xlab="",ylab="", xaxt="n", yaxt="n",
+     pch=16,cex.main=2,main=("Calibration Line"))
+title(xlab="Outside Well", line=2.5, cex.lab=2, font.lab=1)   # move up/down
+title(ylab="In Probe", line=2.3, cex.lab=2, font.lab=1)
 
-abline(lm(y~x), col="black", lty=3,lwd=2)
-lines(xconfint, conf_interval[,2], col="black", lty=2)
-lines(xconfint, conf_interval[,3], col="black", lty=2)
+# Axis major ticks
+axis(1, at = pretty(x, n = 8))
+axis(2, at = pretty(y, n = 8))
+# Optional axis minor ticks
+#axis(1, at = pretty(x, n = 20), tcl = -0.25, labels = FALSE)
+#axis(2, at = pretty(y, n = 20), tcl = -0.25, labels = FALSE)
+
+abline(lm(y~x), col="black", lty=3, lwd=2)
+lines(xconfint, conf_interval[,2], col="#00CD00", lty=2, lwd=4)
+lines(xconfint, conf_interval[,3], col="black", lty=2, lwd=2)
 
 # R-squared
 rsq <- summary(regcurrent)$r.squared
@@ -71,8 +79,17 @@ b_up<-round(min(ci$upr),4)
 
 # Plot calibration thresholds
 plot(x,y,
-     xlab="Outside Well", ylab="In Probe",
+     xlab="", ylab="", xaxt="n", yaxt="n",
      pch=16,xlim=c(0,2*mdl),ylim=c(0,2*b_up))
+title(xlab="Outside Well", line=2.5, cex.lab=2, font.lab=1)   # move up/down
+title(ylab="In Probe", line=2.3, cex.lab=2, font.lab=1)
+
+# Axis major ticks
+axis(1, at = pretty(c(0,2*mdl), n = 6))
+axis(2, at = pretty(c(0,2*b_up), n = 6))
+# Optional axis minor ticks
+#axis(1, at = pretty(c(0,2*mdl), n = 20), tcl = -0.25, labels = FALSE)
+#axis(2, at = pretty(c(0,2*b_up), n = 20), tcl = -0.25, labels = FALSE)
 
 # Build multi-colored title
 mtext("Calibration Thresholds", side=3, line=1, col="black", font=2, cex=2)
