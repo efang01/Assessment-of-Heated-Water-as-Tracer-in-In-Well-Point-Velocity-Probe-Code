107978f9fd080737b0f8fbcec1e403f6d1c63606
Ellis Fangmann <168651076+efang01@users.noreply.github.com>
2026-04-22 10:45:29 -0500

    Add files via upload

--- Calibration Analysis of Data Sets.R
+++ Calibration Analysis of Data Sets.R
@@ -1,74 +1,93 @@
 ################################################################################
 #
-#
 #       Calibration curves for IWPVP
 #       Written by J.F. Devlin 
 #       Modified by Ellis Fangmann
-#       Feb 22, 2026
-#
+#       April 16, 2026
 #
 ################################################################################
-inputpvp<-read.csv("FileName.csv", stringsAsFactors = F)
+
+# Load and prepare data
+inputpvp<-read.csv("1ChannelDataExpDAY.csv", stringsAsFactors = F)
 x<-(inputpvp$SandFlux)
 y<-(inputpvp$ProbeFlux)
 
-
 regcurrent<-lm(formula=y~x)
-m<-round(regcurrent$coefficients[2],1)
-#Vector for calculating confidence intervals
-xconfint<-seq(0,70,by=0.1)
 
-conf_interval <- predict(regcurrent, newdata=(data.frame(x=xconfint)), interval="confidence",
-                         level = 0.95)
+# Extract slope and intercept
+b <- round(regcurrent$coefficients[1], 4)  # intercept
+m <- round(regcurrent$coefficients[2], 4)  # slope
+
+# Vector for calculating confidence intervals
+xconfint<-seq(0,70,by=0.0001)
+
+conf_interval <- predict(regcurrent, newdata=(data.frame(x=xconfint)), 
+                         interval="confidence", level = 0.95)
 
 par(mfrow=c(1,2))
-plot(x,y, xlab="outside well", ylab="in probe",
-     pch=16,main=paste("calibration factor (slope) = ",m))
+
+plot(x,y, xlab="Outside Well", ylab="In Probe",
+     pch=16,cex.main=2,main=paste("Calibration Line"))
+
 abline(lm(y~x),lty=3,lwd=2)
 lines(xconfint, conf_interval[,2], col="black", lty=2)
 lines(xconfint, conf_interval[,3], col="black", lty=2)
+
+# R-squared
 rsq <- summary(regcurrent)$r.squared
-rsq
-text(10, 1050, pos = 4, paste("R squared =", round(rsq, digits = 3) ) )
+text(min(x), max(y)*0.95, pos=4, font=4, paste("R² =",(rsq)))
+
+# Equation label (handles + / - automatically)
+eqn <- paste0("y = ", m, "x ", ifelse(b >= 0, "+ ", "- "), abs(b))
+text(min(x), max(y)*0.98, pos=4, font=4, eqn)
 
 ci<-as.data.frame(conf_interval)
 ci$x<-xconfint
 ci<-ci[c(4,1,2,3)]
 
-#Determine MDL
+# Determine MDL
 b_up<-min(ci$upr)
-#find MDL
-i=1
+
+# Find MDL
+i=11
 while(ci[i,3]<b_up){
   yy<-ci[i+1,3]
-  mdl<<-round(ci[i+1,1],1)
+  mdl<<-round(ci[i+1,1],4)
   i=i+1
 }
 mdl
 
-#Find limit of quantification (threshold value)
+# Find limit of quantification (threshold value)
 i=1
 while(ci[i,2]<b_up){
   yy<-ci[i+1,2]
-  tc<<-round(ci[i+1,1],1)
+  tc<<-round(ci[i+1,1],4)
   i=i+1
 }
 tc
-b_up<-round(min(ci$upr),1)
 
+b_up<-round(min(ci$upr),4)
+
+# Plot calibration thresholds
 plot(x,y,
-     #pch=16,xlim=c(0,2*mdl),ylim=c(0,2*b_up),
-     pch=16,xlim=c(0,0.005),ylim=c(0,2*b_up),
-     main=paste("LoQ = ",tc,
-                " MDL = ",mdl,"\n"," probe mdl  =",b_up))
-lines(xconfint, ci[,2], col="black", lty=2,
-      xlim=c(0,2*mdl))
-lines(xconfint, ci[,3], col="black", lty=2,
-      xlim=c(0,2*mdl))
-lines(xconfint, ci[,4], col="black", lty=2,
-      xlim=c(0,2*mdl),ylim=c(0,2*b_up))
-abline(h=b_up,col="lightgray")
-abline(v=tc,col="pink")
-abline(v=mdl,col="lightblue")
+     xlab="Outside Well", ylab="In Probe",
+     pch=16,xlim=c(0,2*mdl),ylim=c(0,2*b_up))
 
+# Build multi-colored title
+mtext("Calibration Thresholds", side=3, line=1, col="black", font=2, cex=2)
+mtext(paste("LoQ =", tc), side=3, line=0, col="red", font=4, cex=1.3, adj=0)
+mtext(paste("MDL =", mdl), side=3, line=0, col="blue", font=4, cex=1.3 ,adj=0.5)
+mtext(paste("ProbeMDL =", b_up), side=3, line=0, col="black", font=4, cex=1.3, adj=1)
+
+#Pplot upper and lower bounds
+lines(xconfint, ci[,2], col="black", lty=2)
+lines(xconfint, ci[,3], col="#00CD00", lty=2)
+lines(xconfint, ci[,4], col="#00CD00", lty=2)
+
+# Plot probe mdl, loq, mdl
+abline(h=b_up,col="black",lty=1)
+abline(v=tc,col="red",lty=1)
+abline(v=mdl,col="blue",lty=1)
+
+# Print coefficients
 summary(regcurrent)
